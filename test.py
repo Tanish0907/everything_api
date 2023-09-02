ee192b304d57293dea731aa274ec3a5d34917fe7
tarun.kumar <tarun.kumar@bigohtech.com>
2023-09-03 00:33:31 +0530

    threads_test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
-manga_res = {}
+from multiprocessing.dummy import Pool as ThreadPool
+# manga_res = {}
 
 '''
         ln = requests.get(i["link"]).text
@@ -23,62 +24,93 @@ manga_res = {}
 #     page = soup.find_all("img")
 #     for i in page:
 #         print(i)
+# ch_dict = {}
+
+
+# def get_chapters(ch_list):
+#     for i in ch_list:
+#         ch = i.split("-")[-1]
+#         ln = requests.get(i).text
+#         soup = bs(ln, 'lxml')
+#         page = soup.find('p')
+#         mang_page_link = list(page.text.split(","))
+#         ch_dict[ch] = mang_page_link
+#         ch = ch+1
+#     print(ch_dict)
+
+
+# def get_manga(manga_name: str):
+#     manga_name = manga_name.replace(" ", "-")
+#     ch_dict = []
+#     manga = requests.get(f'https://mangapanda.in/manga/{manga_name}').text
+#     soup = bs(manga, 'lxml')
+#     chapters = soup.find_all('li', class_='row')
+#     for i in chapters:
+#         l = i.find('a')
+#         l = l.get('href')
+#         ch_dict.append(l)
+#     return ch_dict
+
+
+# def search(keyword: str = None):
+#     if keyword == None:
+#         return {"Error": "enter a keyword"}
+#     link = f'https://mangapanda.in/search?q={keyword}'
+#     r = requests.get(link).text
+#     soup = bs(r, "lxml")
+#     manga_results = soup.find_all("a", class_="tooltips")
+#     manga_ind = 0
+#     for i in manga_results:
+#         manga = {}
+#         manga["title"] = i["title"]
+#         manga["cover"] = i.find("img")["src"]
+#         ln = requests.get(i.get("href")).text
+#         soup = bs(ln, 'lxml')
+#         chapters = soup.find_all('li', class_='row')
+#         ch_list = []
+#         for j in chapters:
+#             l = j.find('a')
+#             l = l.get('href')
+#             ch_list.append(l)
+#         manga["chapters"] = ch_list
+
+#         manga_res[manga_ind] = manga
+#         manga_ind += 1
+#     print(manga_res)
+
+
+# search("jujutsu")
+# get_chapters("https://mangapanda.in/baki-dou-2018-chapter-3")
+# ch = get_manga("baki-dou-2018")
+# get_chapters(ch)
+pool = ThreadPool(10)
 ch_dict = {}
 
 
-def get_chapters(ch_list):
-    for i in ch_list:
-        ch = i.split("-")[-1]
-        ln = requests.get(i).text
-        soup = bs(ln, 'lxml')
-        page = soup.find('p')
-        mang_page_link = list(page.text.split(","))
-        ch_dict[ch] = mang_page_link
-        ch = ch+1
-    print(ch_dict)
+def get_chapters(link):
+    ch = link.split("-")[-1]
+    ln = requests.get(link).text
+    soup = bs(ln, 'lxml')
+    page = soup.find('p')
+    mang_page_link = list(page.text.split(","))
+    ch_dict[ch] = mang_page_link
 
 
 def get_manga(manga_name: str):
     manga_name = manga_name.replace(" ", "-")
-    ch_dict = []
+    ch_list = []
     manga = requests.get(f'https://mangapanda.in/manga/{manga_name}').text
     soup = bs(manga, 'lxml')
     chapters = soup.find_all('li', class_='row')
     for i in chapters:
         l = i.find('a')
         l = l.get('href')
-        ch_dict.append(l)
-    return ch_dict
+        ch_list.append(l)
+    return ch_list
 
 
-def search(keyword: str = None):
-    if keyword == None:
-        return {"Error": "enter a keyword"}
-    link = f'https://mangapanda.in/search?q={keyword}'
-    r = requests.get(link).text
-    soup = bs(r, "lxml")
-    manga_results = soup.find_all("a", class_="tooltips")
-    manga_ind = 0
-    for i in manga_results:
-        manga = {}
-        manga["title"] = i["title"]
-        manga["cover"] = i.find("img")["src"]
-        ln = requests.get(i.get("href")).text
-        soup = bs(ln, 'lxml')
-        chapters = soup.find_all('li', class_='row')
-        ch_list = []
-        for j in chapters:
-            l = j.find('a')
-            l = l.get('href')
-            ch_list.append(l)
-        manga["chapters"] = ch_list
+res = pool.map(get_chapters, get_manga("baki-dou-2018"))
+print(pool.close())
+print(pool.join())
 
-        manga_res[manga_ind] = manga
-        manga_ind += 1
-    print(manga_res)
-
-
-search("jujutsu")
-# get_chapters("https://mangapanda.in/baki-dou-2018-chapter-3")
-# ch = get_manga("baki-dou-2018")
-# get_chapters(ch)
+print(ch_dict)
